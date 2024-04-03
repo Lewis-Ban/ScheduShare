917113324487586b1479113cdf4633fbad2a2c46
Tomo Jozinovic <tomislavjozinovic@gmail.com>
2024-04-03 13:25:06 -0700

    Fixing location of "Forget ID/Password" button to bottom right of screen

--- app/src/main/java/com/example/schedushare/FriendCalendar.kt
+++ app/src/main/java/com/example/schedushare/FriendCalendar.kt
@@ -1,4 +1,23 @@
 package com.example.schedushare
 
-class FriendCalendar {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class FriendCalendar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.friend_calendar_page)
+
+        val goBackButton = findViewById<Button>(R.id.friend_calendar_button)
+
+        // Click listener
+        goBackButton.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
